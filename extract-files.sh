662a4218400b11145cf681e3242bfb59f967c6db
Sein Lin <seinlin.maung@gmail.com>
2015-05-18 11:23:15 +0800

    Update extract-files.sh and move common things form aries.

--- extract-files.sh
+++ extract-files.sh
@@ -215,94 +215,108 @@ copy_local_files()
 # 	"
 # 
 # copy_files "$COMMON_ROOT_SBIN" "root/sbin" "root"
-copy_files_glob "*.png" "root/res/images/charger" "root"
+# system/core/healthd
+# copy_files_glob "*.png" "root/res/images/charger" "root"
+#
+# libnfc-nci.so
+# libnfc_ndef.so
+# libloc_xtra.so
 
-#libnfc_ndef.so
-#libloc_xtra.so
+# libcnefeatureconfig.so
+# libgps.utils.so
+# libloc_api_v02.so
+# libloc_core.so
+# libloc_ds_api.so
+# libloc_eng.so
+# libmmcamera_interface.so
+# libmmjpeg_interface.so
+# libqomx_core.so
+# libpin-cache.so
+# libstartup-reason.so
+# libkeyctrl.so
+# libcald_client.so
+# libcald_server.so
+# libcald_pal.so
+# libcald_util.so
+# libexcal_core.so
+# libcacao_common.so
+# libcacao_pal.so
+# libcammw.so
+# libcameralight.so
+# libexcal_system.so
+# libcacao_system.so
+# libcacao_parammgr.so
+# libcacao_chokoball.so
+# libcacao_client.so
+# libcacao_face.so
+# libcacao_imageconv.so
+# libcacao_imgproc.so
+# libcacao_jpeg.so
+# libcacao_service.so
+# libsony_face.so
+# libsony_chokoballrsc.so
+# libsomc_chokoballpal.so
+# libsony_chokoball.so
+# libidd.so
+# libprotobuf-c.so
+# libta.so
+# libmiscta.so
+# libcamera_clientsemc.so
+# libxml2.so
+# libtinyalsa.so
+# libaudioroute.so
+# libhardware_legacy.so
+# libsonydseehxwrapper.so
+# libcredential-manager-keystore.so
+# libcredential-manager-service.so
+# lib_asb_tee.so
+# lib_get_secure_mode.so
+# lib_get_rooting_status.so
+# liblights-core.so
+# libsys-utils.so
+# libEffectOmxCore.so
+# libdtcpipplayer.so
+# libsapporo.so
+# libaudiospring.so
+# libaudioflinger.so
+# libmedia.so
+# libdisplayservice.so
+# libsomc_thermal.so
+# libreference-ril.so
+# librilutils.so
+# libril.so
+
 COMMON_LIBS="
-  libnfc-nci.so
-	libcnefeatureconfig.so
-	libgps.utils.so
-	libloc_api_v02.so
-	libloc_core.so
-	libloc_ds_api.so
-	libloc_eng.so
-	libmmcamera_interface.so
-	libmmjpeg_interface.so
-	libqomx_core.so
-	libpin-cache.so
-	libstartup-reason.so
-	libkeyctrl.so
-	libcald_client.so
-	libcald_server.so
-	libcald_pal.so
-	libcald_util.so
-	libexcal_core.so
-	libcacao_common.so
-	libcacao_pal.so
-	libcammw.so
-	libcameralight.so
-	libexcal_system.so
-	libcacao_system.so
-	libcacao_parammgr.so
-	libcacao_chokoball.so
-	libcacao_client.so
-	libcacao_face.so
-	libcacao_imageconv.so
-	libcacao_imgproc.so
-	libcacao_jpeg.so
-	libcacao_service.so
-	libsony_face.so
-	libsony_chokoballrsc.so
-	libsomc_chokoballpal.so
-	libsony_chokoball.so
-	libidd.so
-	libprotobuf-c.so
-	libta.so
-	libmiscta.so
-	libcamera_clientsemc.so
-	libxml2.so
-	libtinyalsa.so
-	libaudioroute.so
-	libhardware_legacy.so
-	libsonydseehxwrapper.so
-	libcredential-manager-keystore.so
-	libcredential-manager-service.so
-	lib_asb_tee.so
-	lib_get_secure_mode.so
-	lib_get_rooting_status.so
-	liblights-core.so
-	libsys-utils.so
-	libEffectOmxCore.so
-	libdtcpipplayer.so
-	libsapporo.so
-	libaudiospring.so
-	libaudioflinger.so
-	libmedia.so
-	libdisplayservice.so
-	libsomc_thermal.so
-	libreference-ril.so
-	librilutils.so
-	libril.so
+  libgps.utils.so
+  libloc_core.so
+  libloc_eng.so
+  libtinyalsa.so
+  libaudioroute.so
+  libhardware_legacy.so
+  libaudioflinger.so
+  libmedia.so
+  libreference-ril.so
+  librilutils.so
+  libril.so
 	"
 
 copy_files "$COMMON_LIBS" "system/lib" ""
 
-COMMON_SOUNDFX_LIBS="
-  libsonysweffect.so
-	libvpt51wrapper.so
-	"
-copy_files "$COMMON_SOUNDFX_LIBS" "system/lib/soundfx" "audio"
+# COMMON_SOUNDFX_LIBS="
+#   libsonysweffect.so
+# 	libvpt51wrapper.so
+# 	"
+# copy_files "$COMMON_SOUNDFX_LIBS" "system/lib/soundfx" "audio"
 
-copy_files_glob "*.so" "system/lib/camera" "camera"
-copy_files_glob "*.dat" "system/vendor/camera" "camera"
-copy_files_glob "*.dat" "system/vendor/camera/LGI02BN1" "camera/LGI02BN1"
-copy_files_glob "*.dat" "system/vendor/camera/SEM02BN1" "camera/SEM02BN1"
-copy_files_glob "*.dat" "system/vendor/camera/SOI20BS1" "camera/SOI20BS1"
+# copy_files_glob "*.so" "system/lib/camera" "camera"
+# copy_files_glob "*.dat" "system/vendor/camera" "camera"
+# copy_files_glob "*.dat" "system/vendor/camera/LGI02BN1" "camera/LGI02BN1"
+# copy_files_glob "*.dat" "system/vendor/camera/SEM02BN1" "camera/SEM02BN1"
+# copy_files_glob "*.dat" "system/vendor/camera/SOI20BS1" "camera/SOI20BS1"
 
 # copy_files "effect_init_params" "system/vendor/etc" "audio"
 
-copy_files_glob "*.so" "system/vendor/lib/rfsa/adsp" "audio"
+#copy_files_glob "*.so" "system/vendor/lib/rfsa/adsp" "audio"
 copy_files_glob "*.so" "system/vendor/lib/soundfx" "audio"
 
 COMMON_VENDOR_LIBS_EXCLUDED="
@@ -311,119 +325,134 @@ COMMON_VENDOR_LIBS_EXCLUDED="
 
 copy_files_glob "lib*.so" "system/vendor/lib" "" "$COMMON_VENDOR_LIBS_EXCLUDED"
 
-copy_files_glob "*.png" "system/somc/chargemon/data/msg" "chargemon_data"
-copy_files_glob "*.png" "system/somc/chargemon/data/num" "chargemon_data"
-copy_files_glob "*.png" "system/somc/chargemon/data/scale" "chargemon_data"
+# copy_files_glob "*.png" "system/somc/chargemon/data/msg" "chargemon_data"
+# copy_files_glob "*.png" "system/somc/chargemon/data/num" "chargemon_data"
+# copy_files_glob "*.png" "system/somc/chargemon/data/scale" "chargemon_data"
 
 # bridgemgrd
 # fm_qsoc_patches
-# hci_qcomm_init
 # port-bridge
+# mm-qcamera-daemon
+# addrsetup
+# tad_static
+# adsprpcd
+# charger_monitor
+# location-mq
+# hci_qcomm_init
+# lowi-server
+# mpdecision
+# netmgrd
+# ptt_socket_app
+# qcom-system-daemon
+# qmuxd
+# qseecomd
+# radish
+# rfs_access
+# rmt_storage
+# sensors.qcom
+# xtwifi-client
+# xtwifi-inet-agent
+# iddd
+# imsdatadaemon
+# imsqmidaemon
+# ims_rtp_daemon
+# idd-logreader
+# irsc_util
+# updatemiscta
+# ta_qmi_service
+# ta_param_loader
+# mlog_qmi_service
+# ssrapp
+# ssr_diag
+# sct_service
+# suntrold
+# illumination_service
+# mediaserver
+# scd
+# credmgrd
+# mm-pp-daemon
+# hvdcp
+# display_color_calib
+# chargemon
+# clearpad_fwloader
+# taimport
 COMMON_BINS="
-	adsprpcd
-	charger_monitor
-	location-mq
-	lowi-server
-	mm-qcamera-daemon
-	mpdecision
-	netmgrd
-	ptt_socket_app
-	qcom-system-daemon
-	qmuxd
-	qseecomd
-	radish
-	rfs_access
-	rmt_storage
-	sensors.qcom
-	xtwifi-client
-	xtwifi-inet-agent
-	iddd
-	imsdatadaemon
-	imsqmidaemon
-	ims_rtp_daemon
-	idd-logreader
-	irsc_util
-	updatemiscta
-	ta_qmi_service
-	ta_param_loader
-	mlog_qmi_service
-	ssrapp
-	ssr_diag
-	sct_service
-	suntrold
-	illumination_service
-	mediaserver
-	scd
-	credmgrd
-	mm-pp-daemon
-	hvdcp
-	display_color_calib
-	chargemon
-	clearpad_fwloader
-	taimport
+  addrsetup
+  tad_static
+  hci_qcomm_init
+  netmgrd
+  qmuxd
+  qseecomd
+  rmt_storage
+  sensors.qcom
+  irsc_util
+  ta_qmi_service
+  mlog_qmi_service
+  sct_service
+  mediaserver
+  hvdcp
 	"
 
 copy_files "$COMMON_BINS" "system/bin" ""
 
+# All the files can be built from device/sony/*
+# COMMON_HW="
+# 	camera.qcom.so
+# 	gps.default.so
+# 	keystore.qcom.so
+# 	lights.default.so
+# 	libdisplay.default.so
+# 	"
+# copy_files "$COMMON_HW" "system/lib/hw" "hw"
+# 
+# COMMON_ETC="
+#   ad_calib.cfg
+#   audio_effects.conf
+#   audio_policy.conf
+#   gps.conf
+#   lowi.conf
+#   media_codecs.xml
+#   mixer_paths.xml
+#   mixer_paths_auxpcm.xml
+#   xtwifi.conf
+#   flashled_calc_parameters.cfg
+#   iddd.conf
+#   dsx_param_file.bin
+#   sap.conf
+#   sec_config
+#   sensor_def_qcomdev.conf
+#   ramdump_ssr.xml
+#   pre_hw_config.sh
+# 	"
+# copy_files "$COMMON_ETC" "system/etc" "etc"
+# 
+# copy_files_glob "*" "system/etc/tfa98xx" "audio"
+# copy_files_glob "*.bin" "system/etc/sforce" "audio"
 
-COMMON_HW="
-	camera.qcom.so
-	gps.default.so
-	keystore.qcom.so
-	lights.default.so
-	libdisplay.default.so
-	"
-copy_files "$COMMON_HW" "system/lib/hw" "hw"
-
-COMMON_ETC="
-  ad_calib.cfg
-  audio_effects.conf
-  audio_policy.conf
-  gps.conf
-  lowi.conf
-  media_codecs.xml
-  mixer_paths.xml
-  mixer_paths_auxpcm.xml
-  xtwifi.conf
-  flashled_calc_parameters.cfg
-  iddd.conf
-  dsx_param_file.bin
-  sap.conf
-  sec_config
-  sensor_def_qcomdev.conf
-  ramdump_ssr.xml
-  libnfc-brcm.conf
-  libnfc-nxp.conf
-  pre_hw_config.sh
-	"
-copy_files "$COMMON_ETC" "system/etc" "etc"
-
-copy_files_glob "*" "system/etc/tfa98xx" "audio"
-copy_files_glob "*.bin" "system/etc/sforce" "audio"
-
-COMMON_ETC_ACDBDATA_Fluid="
-	Fluid_Bluetooth_cal.acdb
-	Fluid_General_cal.acdb
-	Fluid_Global_cal.acdb
-	Fluid_Handset_cal.acdb
-	Fluid_Hdmi_cal.acdb
-	Fluid_Headset_cal.acdb
-	Fluid_Speaker_cal.acdb
-	"
-
-copy_files "$COMMON_ETC_ACDBDATA_Fluid" "system/etc/acdbdata/Fluid" "etc/acdbdata/Fluid"
-
-COMMON_ETC_ACDBDATA_Liquid="
-	Liquid_Bluetooth_cal.acdb
-	Liquid_General_cal.acdb
-	Liquid_Global_cal.acdb
-	Liquid_Handset_cal.acdb
-	Liquid_Hdmi_cal.acdb
-	Liquid_Headset_cal.acdb
-	Liquid_Speaker_cal.acdb
-	"
-
-copy_files "$COMMON_ETC_ACDBDATA_Liquid" "system/etc/acdbdata/Liquid" "etc/acdbdata/Liquid"
+# These files are not included in aosp 5.1 build.
+# COMMON_ETC_ACDBDATA_Fluid="
+# 	Fluid_Bluetooth_cal.acdb
+# 	Fluid_General_cal.acdb
+# 	Fluid_Global_cal.acdb
+# 	Fluid_Handset_cal.acdb
+# 	Fluid_Hdmi_cal.acdb
+# 	Fluid_Headset_cal.acdb
+# 	Fluid_Speaker_cal.acdb
+# 	"
+# 
+# copy_files "$COMMON_ETC_ACDBDATA_Fluid" "system/etc/acdbdata/Fluid" "etc/acdbdata/Fluid"
+# 
+# COMMON_ETC_ACDBDATA_Liquid="
+# 	Liquid_Bluetooth_cal.acdb
+# 	Liquid_General_cal.acdb
+# 	Liquid_Global_cal.acdb
+# 	Liquid_Handset_cal.acdb
+# 	Liquid_Hdmi_cal.acdb
+# 	Liquid_Headset_cal.acdb
+# 	Liquid_Speaker_cal.acdb
+# 	"
+# 
+# copy_files "$COMMON_ETC_ACDBDATA_Liquid" "system/etc/acdbdata/Liquid" "etc/acdbdata/Liquid"
 
 COMMON_ETC_ACDBDATA_MTP="
 	MTP_Bluetooth_cal.acdb
@@ -437,12 +466,12 @@ COMMON_ETC_ACDBDATA_MTP="
 
 copy_files "$COMMON_ETC_ACDBDATA_MTP" "system/etc/acdbdata/MTP" "etc/acdbdata/MTP"
 
-COMMON_IDC=clearpad.idc
-
-copy_files "$COMMON_IDC" "system/usr/idc" "hw"
-
-COMMON_AUDIO="
-	"
+# COMMON_IDC=clearpad.idc
+# 
+# copy_files "$COMMON_IDC" "system/usr/idc" "hw"
+# 
+# COMMON_AUDIO="
+# 	"
 #copy_files "$COMMON_AUDIO" "system/lib" "audio"
 
 # COMMON_EGL="
@@ -456,108 +485,113 @@ COMMON_VENDOR_NFC="
 copy_files "$COMMON_VENDOR_NFC" "system/vendor/firmware" "nfc"
 
 COMMON_VENDOR_EGL="
-	libEGL_adreno.so
-	libGLESv1_CM_adreno.so
-	libGLESv2_adreno.so
+  eglsubAndroid.so
+  libEGL_adreno.so
+  libGLESv1_CM_adreno.so
+  libGLESv2_adreno.so
+  libq3dtools_adreno.so
 	"
 copy_files "$COMMON_VENDOR_EGL" "system/vendor/lib/egl" "egl"
 
+# flp.default.so
+# power.qcom.so
 COMMON_VENDOR_HW="
-	flp.default.so
-	power.qcom.so
 	sensors.msm8974.so
-        "
+  "
 copy_files "$COMMON_VENDOR_HW" "system/vendor/lib/hw" "hw"
 
-COMMON_FIRMWARE="
-	a330_pfp.fw
-	a330_pm4.fw
-	adsp.b00
-	adsp.b01
-	adsp.b02
-	adsp.b03
-	adsp.b04
-	adsp.b05
-	adsp.b06
-	adsp.b07
-	adsp.b08
-	adsp.b09
-	adsp.b10
-	adsp.b11
-	adsp.b12
-	adsp.mdt
-	BCM43xx.hcd
-	cmnlib.b00
-	cmnlib.b01
-	cmnlib.b02
-	cmnlib.b03
-	cmnlib.flist
-	cmnlib.mdt
-	cpp_firmware_v1_2_0.fw
-	mba.b00
-	mba.mdt
-	modem.b00
-	modem.b01
-	modem.b02
-	modem.b03
-	modem.b06
-	modem.b08
-	modem.b09
-	modem.b11
-	modem.b12
-	modem.b13
-	modem.b14
-	modem.b15
-	modem.b16
-	modem.b17
-	modem.b18
-	modem.b19
-	modem.b22
-	modem.b23
-	modem.b24
-	modem.b25
-	modem.mdt
-	tzhdcp.b00
-	tzhdcp.b01
-	tzhdcp.b02
-	tzhdcp.b03
-	tzhdcp.flist
-	tzhdcp.mdt
-	tzlibasb.b00
-	tzlibasb.b01
-	tzlibasb.b02
-	tzlibasb.b03
-	tzlibasb.flist
-	tzlibasb.mdt
-	tznautilus.b00
-	tznautilus.b01
-	tznautilus.b02
-	tznautilus.b03
-	tznautilus.flist
-	tznautilus.mdt
-	tzsuntory.b00
-	tzsuntory.b01
-	tzsuntory.b02
-	tzsuntory.b03
-	tzsuntory.flist
-	tzsuntory.mdt
-	tzwidevine.b00
-	tzwidevine.b01
-	tzwidevine.b02
-	tzwidevine.b03
-	tzwidevine.mdt
-	venus.b00
-	venus.b01
-	venus.b02
-	venus.b03
-	venus.b04
-	venus.mbn
-	venus.mdt
-	"
-copy_files "$COMMON_FIRMWARE" "system/etc/firmware" "etc/firmware"
+# COMMON_FIRMWARE="
+# 	a330_pfp.fw
+# 	a330_pm4.fw
+# 	adsp.b00
+# 	adsp.b01
+# 	adsp.b02
+# 	adsp.b03
+# 	adsp.b04
+# 	adsp.b05
+# 	adsp.b06
+# 	adsp.b07
+# 	adsp.b08
+# 	adsp.b09
+# 	adsp.b10
+# 	adsp.b11
+# 	adsp.b12
+# 	adsp.mdt
+# 	BCM43xx.hcd
+# 	cmnlib.b00
+# 	cmnlib.b01
+# 	cmnlib.b02
+# 	cmnlib.b03
+# 	cmnlib.flist
+# 	cmnlib.mdt
+# 	cpp_firmware_v1_2_0.fw
+# 	mba.b00
+# 	mba.mdt
+# 	modem.b00
+# 	modem.b01
+# 	modem.b02
+# 	modem.b03
+# 	modem.b06
+# 	modem.b08
+# 	modem.b09
+# 	modem.b11
+# 	modem.b12
+# 	modem.b13
+# 	modem.b14
+# 	modem.b15
+# 	modem.b16
+# 	modem.b17
+# 	modem.b18
+# 	modem.b19
+# 	modem.b22
+# 	modem.b23
+# 	modem.b24
+# 	modem.b25
+# 	modem.mdt
+# 	tzhdcp.b00
+# 	tzhdcp.b01
+# 	tzhdcp.b02
+# 	tzhdcp.b03
+# 	tzhdcp.flist
+# 	tzhdcp.mdt
+# 	tzlibasb.b00
+# 	tzlibasb.b01
+# 	tzlibasb.b02
+# 	tzlibasb.b03
+# 	tzlibasb.flist
+# 	tzlibasb.mdt
+# 	tznautilus.b00
+# 	tznautilus.b01
+# 	tznautilus.b02
+# 	tznautilus.b03
+# 	tznautilus.flist
+# 	tznautilus.mdt
+# 	tzsuntory.b00
+# 	tzsuntory.b01
+# 	tzsuntory.b02
+# 	tzsuntory.b03
+# 	tzsuntory.flist
+# 	tzsuntory.mdt
+# 	tzwidevine.b00
+# 	tzwidevine.b01
+# 	tzwidevine.b02
+# 	tzwidevine.b03
+# 	tzwidevine.mdt
+# 	venus.b00
+# 	venus.b01
+# 	venus.b02
+# 	venus.b03
+# 	venus.b04
+# 	venus.mbn
+# 	venus.mdt
+# 	"
+# copy_files "$COMMON_FIRMWARE" "system/etc/firmware" "etc/firmware"
 
-copy_files_glob "*.bin" "system/etc/firmware/wcd9320" "audio"
-copy_files_glob "*" "system/etc/firmware/wlan/bcmdhd" "etc/firmware"
+copy_files_glob "*" "system/etc/firmware" "etc/firmware"
+copy_files_glob "*" "system/vendor/firmware" "etc/firmware"
+
+# copy_files_glob "*.bin" "system/etc/firmware/wcd9320" "audio"
+# copy_files_glob "*" "system/etc/firmware/wlan/bcmdhd" "etc/firmware"
 
 COMMON_VENDOR_FIRMWARE="
 	keymaster.b00
@@ -569,6 +603,6 @@ COMMON_VENDOR_FIRMWARE="
 
 copy_files "$COMMON_VENDOR_FIRMWARE" "system/vendor/firmware/keymaster" "etc/firmware"
 
-echo $BASE_PROPRIETARY_DEVICE_DIR/libcnefeatureconfig.so:obj/lib/libcnefeatureconfig.so \\ >> $BLOBS_LIST
-echo $BASE_PROPRIETARY_DEVICE_DIR/hw/lights.default.so:system/lib/hw/lights.msm8974.so \\ >> $BLOBS_LIST
+# echo $BASE_PROPRIETARY_DEVICE_DIR/libcnefeatureconfig.so:obj/lib/libcnefeatureconfig.so \\ >> $BLOBS_LIST
+# echo $BASE_PROPRIETARY_DEVICE_DIR/hw/lights.default.so:system/lib/hw/lights.msm8974.so \\ >> $BLOBS_LIST
 
